6f7a4ba68cb730b21b876aab6ffe28cc990c1484
Hugo David Farji <hdf1986.6700@gmail.com>
2017-04-01 20:31:04 -0300

    Fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :lights, only: [:index] do
         collection do
-          put :toggle
-          put :view
+          get :toggle
+          get :view
         end
       end
     end
